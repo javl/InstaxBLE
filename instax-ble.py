c8c75e78d81bb84e4e5c8a7f82fcdeaea743c955
javl <mail@jaspervanloenen.com>
2022-12-14 19:08:04 +0100

    add initial testing script

--- instax-ble.py
+++ instax-ble.py
@@ -3,45 +3,51 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 from bleak.backends.characteristic import BleakGATTCharacteristic
 from struct import pack, unpack_from
+from events import EventType
+
+import logging
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
 
 writeUUID = '70954783-2d83-473d-9e5f-81e1d02d5273'
 notifyUUID = '70954784-2d83-473d-9e5f-81e1d02d5273'
 
 def prettify_bytearray(value):
-  return ' '.join([f'{x:02x}' for x in value])
+    return ' '.join([f'{x:02x}' for x in value])
 
 def parseAccelerometer(data):
-  x, y, z, o = unpack_from('>hhhB', data[3:])
-  print(f'x: {x}, y: {y}, z: {z}, o: {o}')
+    # if len(data) < 16:
+    #     _logger.error(f"Response packet size should be >= 16 (was {len(data)})");
+    #     return
 
-data = b'aB\x00\x0f0\x00\x00\x03\xe0\xff\xe0\x00\x10\x00K'
+    unknown, x, y, z, o = unpack_from('>BhhhB', data)
+    print(f'x: {x}, y: {y}, z: {z}, o: {o}')
+
+# data = b'aB\x00\x0f0\x00\x00\x03\xe0\xff\xe0\x00\x10\x00K'
 # parseAccelerometer(data)
-print(prettify_bytearray(data))  # header
+# print(prettify_bytearray(data))  # header
 
-header, length, i, k, x, y, z, o = unpack_from('<HHBBhhhB', data)
-print('header: ', header, '\t', prettify_bytearray(data[0:2]))
-print('length: ', length, '\t', prettify_bytearray(data[2:4]))
-print('i: ', i, '\t\t', prettify_bytearray(data[4:5]))
-print('k: ', k, '\t\t', prettify_bytearray(data[5:6]))
-print('x: ', x, '\t', prettify_bytearray(data[6:8]))
-print('y: ', y, '\t', prettify_bytearray(data[8:10]))
-print('z: ', z, '\t', prettify_bytearray(data[10:12]))
-print('o: ', o, '\t\t', prettify_bytearray(data[12:13]))
-
-# print(prettify_bytearray(data[0:2]))  # header
-# print(prettify_bytearray(data[3:4]))  # hlength
-# exit()
+# header, length, i, k, x, y, z, o = unpack_from('<HHBBhhhB', data)
+# _logger.info(f' header: {header} \t{prettify_bytearray(data[0:2])}')
+# _logger.info(f' length: {length} \t{prettify_bytearray(data[2:4])}')
+# _logger.info(f' i: {i} \t\t{prettify_bytearray(data[4:5])}')
+# _logger.info(f' k: {k} \t\t{prettify_bytearray(data[5:6])}')
+# _logger.info(f' x: {x} \t\t{prettify_bytearray(data[6:8])}')
+# _logger.info(f' y: {y} \t\t{prettify_bytearray(data[8:10])}')
+# _logger.info(f' z: {z} \t\t{prettify_bytearray(data[10:12])}')
+# _logger.info(f' o: {o} \t\t{prettify_bytearray(data[12:13])}')
 
 def createChecksum(bytearray):
     return (255 - (sum(bytearray) & 255)) & 255;
 
-def createPacket(payload):
-    start = b'\x41\x62'  # Ab from client, server responds with Ba
-    packetSize = pack('>H', 5 + len(payload))
-    packet = start + packetSize + payload
+def createPacket(eventType: EventType, payload:bytes=b''):
+    header = b'\x41\x62'  # 'Ab' from client to printer, 'aB' from printer to client
+    opCode = bytes(eventType.value)
+    packetSize = pack('>H', 7 + len(payload))
+    packet = header + packetSize + opCode + payload
     packet += pack('B', createChecksum(packet))
-    print(f"Sending packet: {packet}, (length: {len(packet)})")
-    print(f"  {prettify_bytearray(packet)} (length: {len(packet)}")
+    _logger.info(f"Sending packet: {packet}, (length: {len(packet)})")
+    _logger.info(f"  {prettify_bytearray(packet)} (length: {len(packet)}")
     return packet
 
 def validate_checksum(data):
@@ -49,25 +55,34 @@ def validate_checksum(data):
 
 def notification_handler(characteristic: BleakGATTCharacteristic, packet: bytearray):
     if len(packet) < 8:
-        print(f"Error: response packet size should be >= 8 (was {len(packet)})")
-        raise
+        _logger.error(f"Error: response packet size should be >= 8 (was {len(packet)})")
+        return
     elif not validate_checksum(packet):
-        print("Checksum validation failed")
-        raise
+        _logger.error("Checksum validation failed")
+        return
 
-    header, length, op1, op2 = unpack_from('<HHBB', packet)
-    print('header: ', header, '\t', prettify_bytearray(packet[0:2]))
-    print('length: ', length, '\t', prettify_bytearray(packet[2:4]))
-    print('op1: ', op1, '\t\t', prettify_bytearray(packet[4:5]))
-    print('op2: ', op2, '\t\t', prettify_bytearray(packet[5:6]))
+    header, length, op1, op2 = unpack_from('>HHBB', packet)
+    _logger.info(f'header: {header}\t{prettify_bytearray(packet[0:2])}')
+    _logger.info(f'length: {length}\t{prettify_bytearray(packet[2:4])}')
+    _logger.info(f'op1: {op1}\t\t{prettify_bytearray(packet[4:5])}')
+    _logger.info(f'op2: {op2}\t\t{prettify_bytearray(packet[5:6])}')
 
-    # data = packet[4:-1]  # the packet without header and checksum
-    data = packet[6:-1]  # the packet without headers and checksum
-    print(f'Response data: {data} (length: {len(data)}, checksum OK)')
-    print(f'  {prettify_bytearray(data)}')
+    try:
+        eventType = EventType((op1, op2))
+    except ValueError:
+        _logger.error(f"Unknown event type ({op1}, {op2}")
+        return
 
-cmd_serial = b'\x00\x01\x02'
-cmd_accelerometer = b'\x30\x00'
+    _logger.info(f"eventType: {eventType}")
+    data = packet[6:-1]  # the packet without headers, opcodes and checksum
+    _logger.info(f'response data: {data} (length: {len(data)}, checksum OK)')
+    _logger.info(f'  {prettify_bytearray(data)}')
+
+    match eventType:
+        case EventType.XYZ_AXIS_INFO:
+            parseAccelerometer(data)
+        case _:
+            _logger.info(f"No parser for event type {eventType}")
 
 async def main():
     dev = None
@@ -77,7 +92,7 @@ async def main():
             dev = d
             break
     if not dev:
-        print("No printer found")
+        _logger.error("No printer found")
         exit()
 
     print(f'Found instax printer {dev.name} at {dev.address}')
@@ -86,9 +101,9 @@ async def main():
         print('connected')
         await client.start_notify(notifyUUID, notification_handler)
 
-        packet = createPacket(cmd_serial) # Request serial number
-        await client.write_gatt_char(writeUUID, packet)
-
-        await asyncio.sleep(5.0)
+        packet = createPacket(EventType.XYZ_AXIS_INFO) # Request serial number
+        while True:
+            await client.write_gatt_char(writeUUID, packet)
+            await asyncio.sleep(0.5)
 
 asyncio.run(main())
