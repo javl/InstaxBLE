05c3d2862c1354253b463950198ed98f814da245
javl <mail@jaspervanloenen.com>
2022-12-18 16:35:09 +0100

    add disconnect command to example code

--- InstaxBle.py
+++ InstaxBle.py
@@ -188,3 +188,4 @@ if __name__ == '__main__':
     if instax.isConnected:
         instax.send_led_pattern([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
         instax.print_image('example.jpg')
+    instax.disconnect()
