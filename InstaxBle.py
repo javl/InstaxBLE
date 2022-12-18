de53eb4fd27f300ee925d76beebeb44a66b1d8c3
javl <mail@jaspervanloenen.com>
2022-12-18 16:24:37 +0100

    Initial version of class-based script

---
+++ InstaxBle.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+
+from events import EventType
+from struct import pack
+import asyncio
+from bleak import BleakScanner, BleakClient
+import socket
+from PIL import Image
+from time import sleep
+
+class InstaxBle:
+    def __init__(self, printEnabled=False, printerName=None):
+        """
+        Initialize the InstaxBle class.
+        printEnabled: by default, actual printing is disabled to prevent misprints.
+        printerName: if specified, will only connect to a printer with this name.
+        """
+        self.printEnabled = printEnabled
+        self.isConnected = False
+        self.device = None
+        self.printerName = printerName
+        self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+
+    def connect(self, timeout=0):
+        """ Connect to the printer. Quit trying after timeout seconds. """
+        self.isConnected = False
+        self.device = asyncio.run(self.find_device(timeout=timeout))
+        if self.device:
+            print(f'found device {self.device.name}, connecting...')
+            try:
+                self.sock.connect((self.device.address, 6))
+                self.isConnected = True
+                print('connected')
+            except Exception as e:
+                print(f'connection failed: {e}')
+
+    def disconnect(self):
+        if self.isConnected:
+            self.socket.disconnect()
+
+    def enable_printing(self):
+        """ Enable printing. """
+        self.printEnabled = True
+
+    def disable_printing(self):
+        """ Disable printing. """
+        self.printEnabled = False
+
+    async def find_device(self, timeout=0, mode='ANDROID'):
+        """" Scan for our device and return it when found """
+        print('Looking for instax printer...')
+        secondsTried = 0
+        while True:
+            devices = await BleakScanner.discover(timeout=1)
+            for device in devices:
+                # if (self.printerName is None and dev.name.startswith('INSTAX-') and dev.name.endswith(mode)) or \
+                if (self.printerName is None and device.name.startswith('INSTAX-') and device.name.endswith(f'({mode})')) or \
+                   device.name == self.printerName:
+                    return device
+            secondsTried += 1
+            if timeout != 0 and secondsTried >= timeout:
+                return None
+
+    def create_color_payload(self, colorArray, speed, loop, when):
+        """
+        Create a payload for a color pattern.
+        colorArray: array of RGB values to use in animation, e.g. [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
+        speed: time per frame/color. Higher is slower animation
+        loop: 0 = don't repeat (so play once), 1-254 = times to repeat, 255 = repeat forever
+        when: 0 = normal, 1 = on print, 2 = on print completion, 3 = pattern switch
+        """
+
+        payload = pack('BBBB', when, len(colorArray), speed, loop)
+        for color in colorArray:
+            payload += pack('BBB', color[0], color[1], color[2])
+        return payload
+
+    def send_led_pattern(self, pattern, speed=5, loop=255, when=0):
+        """ Send a LED pattern to the Instax printer. """
+        payload = self.create_color_payload(pattern, speed, loop, when)
+        packet = self.create_packet(EventType.LED_PATTERN_SETTINGS, payload)
+        return self.send_packet(packet)
+
+    def prettify_bytearray(self, value):
+        """ Helper funtion to convert a bytearray to a string of hex values. """
+        return ' '.join([f'{x:02x}' for x in value])
+
+    def create_checksum(self, bytearray):
+        """ Create a checksum for a given packet. """
+        return (255 - (sum(bytearray) & 255)) & 255
+
+    def create_packet(self, eventType, payload=b''):
+        """ Create a packet to send to the printer. """
+        if isinstance(eventType, EventType):  # allows passing in an event or a value directly
+            eventType = eventType.value
+
+        header = b'\x41\x62'  # Ab from client to printer, Ba from printer to client
+        opCode = bytes([eventType[0], eventType[1]])
+        packetSize = pack('>H', 7 + len(payload))
+        packet = header + packetSize + opCode + payload
+        packet += pack('B', self.create_checksum(packet))
+        return packet
+
+    def validate_checksum(self, packet):
+        """ Validate the checksum of a packet. """
+        return (sum(packet) & 255) == 255
+
+    async def send_color(self):
+        """ send a color pattern """
+        im = Image.open('gradient.jpg')
+        px = im.load()
+        size = im.size if im._getexif().get(274, 0) < 5 else im.size[::-1]
+        colorArray = []
+        for x in range(0, size[0], 20):
+            color = im.getpixel((x, 0))
+            colorArray.append([color[0], color[1], color[2]])
+
+        payload = self.create_color_payload(colorArray, 10, 255)
+        packet = self.create_packet(EventType.LED_PATTERN_SETTINGS, payload)
+
+        sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        sock.connect((self.device.address, 6))
+        sock.send(packet)
+        resp = sock.recv(8)
+        sock.close()
+        return resp
+
+    def send_packet(self, packet):
+        """ Send a packet to the printer. """
+        # print("sending: ", self.prettify_bytearray(packet))
+        if self.isConnected:
+            self.sock.send(packet)
+            # print('sent')
+            response = self.sock.recv(64)
+            self.sock.close()
+            return response
+
+    # TODO: printer doesn't seem to respond to this
+    # def shut_down(self):
+    #     """ Shut down the printer. """
+    #     packet = self.create_packet(EventType.SHUT_DOWN)
+    #     return self.send_packet(packet)
+
+    def image_to_bytes(self, imagePath):
+        """ Convert an image to a bytearray """
+        imgdata = None
+        try:
+            with open(imagePath, "rb") as image:
+                imgdata = bytearray(image.read())
+            return imgdata
+        except Exception as e:
+            print('Error loading image: ', e)
+
+    def print_image(self, imagePath):
+        """ print an image """
+        imgData = self.image_to_bytes(imagePath)
+        printCommands = [
+            self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_START, b'\x02\x00\x00\x00\x00\x00' + pack('>H', len(imgData)))
+        ]
+
+        # divide image data up into chunks of 900 bytes and pad the last chunk with 0s if needed
+        imgDataChunks = [imgData[i:i + 900] for i in range(0, len(imgData), 900)]
+        if len(imgDataChunks[-1]) < 900:
+            imgDataChunks[-1] = imgDataChunks[-1] + bytes(900 - len(imgDataChunks[-1]))
+
+        for index, chunk in enumerate(imgDataChunks):
+            imgDataChunks[index] = pack('>I', index) + chunk  # add chunk number as int (4 bytes)
+            printCommands.append(self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_DATA, pack('>I', index) + chunk))
+
+        printCommands.extend([
+            self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_END),
+            self.create_packet(EventType.PRINT_IMAGE),
+            self.create_packet((0, 2), b'\x02'),
+        ])
+
+        if not self.printEnabled:
+            return "Printing is disabled"
+
+        for index, packet in enumerate(printCommands):
+            print(f'sending image packet {index+1}/{len(printCommands)}')
+            self.send_packet(packet)
+            # print(f'({len(packet)}) {self.prettify_bytearray(packet[:40])}')
+
+
+if __name__ == '__main__':
+    instax = InstaxBle()
+    instax.connect()
+    if instax.isConnected:
+        instax.send_led_pattern([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
+        instax.print_image('testxx.jpg')
--- readme.md
+++ readme.md
@@ -1,14 +1,46 @@
 # Instax-BLE
 
-An attempt at creating a script to control my Instax Mini Link photo printer over bluetooth.
-
-Very much work in progress, not functional yet.
-
-Borrows heavily from notes and ideas as discussed in [this thread](https://github.com/jpwsutton/instax_api/issues/21#issuecomment-1352639100).
-
-Update:
-It works! The final code is not in the online repo yet, but I will add it as soon I've been able to do some cleanup.
-
 ![instax-ble-gif](https://user-images.githubusercontent.com/734644/208236529-550df42b-6d06-46ef-a38b-e683698d9ef6.gif)
+
+### Control your Instax Mini Link printer from Python
+
+This Python module can be used to control your Instax bluetooth printer. Only tested with the Instax Mini Link, but it should also work with other bluetooth Instax models (though you might have to experiment with the image size when using the Square or Wide models).
+
+This code is working, but far from finished or polished. Create an issue if you run into any trouble.
+
+Some notes on the current state of this module:
+
+* The script does not check if your image is the right size or orientation. I've tested printing with imags that are 600x800 pixels and don't know what happens when you send your image in landscape orientation so you might want to rotate it beforehand.
+* For faster (and possibly more reliable) printing remove any exifdata from your image. This will reduce the amount of data that needs to be send to the device.
+
+If you want to control one of the WiFi enabled printers instead, you can use [Instax-api](https://github.com/jpwsutton/instax_api). This script borrows heavily from notes and ideas as discussed in [this](https://github.com/jpwsutton/instax_api/issues/21#issuecomment-1352639100) instax-api thread.
+### Enable printing
+By default printing is disabled so you can test your code without the risk of accidental prints.
+To enable printing, call instax.enable_printing() or specify printingEnabled=True in the constructor.
+
+    instax = InstaxBle(enablePrinting=True)
+    # or
+    instax = InstaxBle()
+    instax.enable_printing()
+
+### Using a specific printer
+By default InstaxBle connects to the first printer it can find. You can (try to) connect to a specific
+printer by specifying its name in the constructor. Note: at this point you need to use the device name that ends in `(Android)`, not `(IOS)`:
+
+`instax = InstaxBle(printerName='Instax-123456(Android)')`
+
+### Sending LED patterns
+Controlling the LED on the printer works by sending patterns: series of colors to be displayed in order. You can tweak these using a couple of extra settings:
+1. frameDuration: how long to show each color for (higher is a slower animation)
+2. repeats: 0 plays the animation once, 1-254 repeats the animation n-times, 255 repeats forever
+3. when: patterns can start playing at different moments. Allowed values are:
+   1. `0` normal
+   2. `1` on print
+   3. `2` on print complete
+   4. `3` pattern swap (not sure what this is)
+
+So for example, to make the LED blink blue:
+
+    send_led_pattern(colors=[[0, 0, 255], [0, 0, 0]], speed=5, loop=255, when=0)
 
 Test pattern image from [Vecteezy](https://www.vecteezy.com/free-vector/test-pattern)
