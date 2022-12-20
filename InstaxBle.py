3f93f7394e6b76aef9aaca7c4a364ecc2ffff093
javl <mail@jaspervanloenen.com>
2022-12-20 13:39:24 +0100

    update code to get rid of the bluetooth sockets and write to gatt directly

--- InstaxBle.py
+++ InstaxBle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 from events import EventType
-from struct import pack
+from struct import pack, unpack_from
 import asyncio
-from bleak import BleakScanner
-import socket
-
+from bleak import BleakScanner, BleakClient
+import LedPatterns
 
 class InstaxBle:
     def __init__(self, printEnabled=False, printerName=None):
@@ -15,27 +14,49 @@ class InstaxBle:
         printerName: if specified, will only connect to a printer with this name.
         """
         self.printEnabled = printEnabled
-        self.isConnected = False
         self.device = None
         self.printerName = printerName
-        self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self.writeUUID = '70954783-2d83-473d-9e5f-81e1d02d5273'
+        self.notifyUUID = '70954784-2d83-473d-9e5f-81e1d02d5273'
+        self.client = BleakClient(None)
 
-    def connect(self, timeout=0):
+    def notification_handler(self, characteristic, packet):
+        if len(packet) < 8:
+            print(f"Error: response packet size should be >= 8 (was {len(packet)})")
+            raise
+        elif not self.validate_checksum(packet):
+            print("Checksum validation error")
+            raise
+
+        # header, length, op1, op2 = unpack_from('<HHBB', packet)
+        # print('header: ', header, '\t', self.prettify_bytearray(packet[0:2]))
+        # print('length: ', length, '\t', self.prettify_bytearray(packet[2:4]))
+        # print('op1: ', op1, '\t\t', self.prettify_bytearray(packet[4:5]))
+        # print('op2: ', op2, '\t\t', self.prettify_bytearray(packet[5:6]))
+
+        # # data = packet[4:-1]  # the packet without header and checksum
+        # data = packet[6:-1]  # the packet without headers and checksum
+        # print(f'Response data: {data} (length: {len(data)}, checksum OK)')
+        # print(f'  {self.prettify_bytearray(data)}')
+
+
+    async def connect(self, timeout=0):
         """ Connect to the printer. Quit trying after timeout seconds. """
-        self.isConnected = False
-        self.device = asyncio.run(self.find_device(timeout=timeout))
+        # self.isConnected = False
+        self.device = await self.find_device(timeout=timeout)
         if self.device:
-            print(f'found device {self.device.name}, connecting...')
             try:
-                self.sock.connect((self.device.address, 6))
-                self.isConnected = True
-                print('connected')
+                self.client = BleakClient(self.device.address)
+                if not self.client.is_connected:
+                    await self.client.connect()
+                await self.client.start_notify(self.notifyUUID, self.notification_handler)
             except Exception as e:
-                print(f'connection failed: {e}')
+                print('error on attaching notification_handler: ', e)
 
-    def disconnect(self):
-        if self.isConnected:
-            self.sock.disconnect()
+    async def disconnect(self):
+        if self.client:
+            if self.client.is_connected:
+                await self.client.disconnect()
 
     def enable_printing(self):
         """ Enable printing. """
@@ -45,14 +66,15 @@ class InstaxBle:
         """ Disable printing. """
         self.printEnabled = False
 
-    async def find_device(self, timeout=0, mode='ANDROID'):
+    async def find_device(self, timeout=0, mode='IOS'):
         """" Scan for our device and return it when found """
         print('Looking for instax printer...')
         secondsTried = 0
         while True:
             devices = await BleakScanner.discover(timeout=1)
             for device in devices:
-                # if (self.printerName is None and dev.name.startswith('INSTAX-') and dev.name.endswith(mode)) or \
+                if device.name.startswith('INSTAX-'):
+                    print(device.name)
                 if (self.printerName is None and device.name.startswith('INSTAX-') and device.name.endswith(f'({mode})')) or \
                    device.name == self.printerName:
                     return device
@@ -104,41 +126,20 @@ class InstaxBle:
         """ Validate the checksum of a packet. """
         return (sum(packet) & 255) == 255
 
-    # async def send_color(self):
-    #     """ send a color pattern """
-    #     im = Image.open('gradient.jpg')
-    #     px = im.load()
-    #     size = im.size if im._getexif().get(274, 0) < 5 else im.size[::-1]
-    #     colorArray = []
-    #     for x in range(0, size[0], 20):
-    #         color = im.getpixel((x, 0))
-    #         colorArray.append([color[0], color[1], color[2]])
-
-    #     payload = self.create_color_payload(colorArray, 10, 255)
-    #     packet = self.create_packet(EventType.LED_PATTERN_SETTINGS, payload)
-
-    #     sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    #     sock.connect((self.device.address, 6))
-    #     sock.send(packet)
-    #     resp = sock.recv(8)
-    #     sock.close()
-    #     return resp
-
-    def send_packet(self, packet):
+    async def send_packet(self, packet):
         """ Send a packet to the printer. """
-        # print("sending: ", self.prettify_bytearray(packet))
-        if self.isConnected:
-            self.sock.send(packet)
-            # print('sent')
-            response = self.sock.recv(64)
-            self.sock.close()
-            return response
+        if self.client:
+            if not self.client.is_connected:
+                await self.client.connect()
+            await self.client.write_gatt_char(self.writeUUID, packet)
+        else:
+            print("no client to send packet to")
 
     # TODO: printer doesn't seem to respond to this
-    # def shut_down(self):
+    # async def shut_down(self):
     #     """ Shut down the printer. """
     #     packet = self.create_packet(EventType.SHUT_DOWN)
-    #     return self.send_packet(packet)
+    #     return await self.send_packet(packet)
 
     def image_to_bytes(self, imagePath):
         """ Convert an image to a bytearray """
@@ -151,12 +152,14 @@ class InstaxBle:
         except Exception as e:
             print('Error loading image: ', e)
 
-    def print_image(self, imgSrc):
-        """ print an image. Either pass a path to an image or a bytearray"""
+    async def print_image(self, imgSrc):
+        """
+        print an image. Either pass a path to an image (as a string) or pass
+        the bytearray to print directly
+        """
+        imgData = imgSrc
         if isinstance(imgSrc, str):  # if it's a path, load the image contents
             imgData = self.image_to_bytes(imgSrc)
-        else:
-            imgData = imgSrc
 
         printCommands = [
             self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_START, b'\x02\x00\x00\x00\x00\x00' + pack('>H', len(imgData)))
@@ -178,20 +181,23 @@ class InstaxBle:
         ])
 
         if not self.printEnabled:
-            return "Printing is disabled"
+            print( "Printing is disabled")
+            return
 
         for index, packet in enumerate(printCommands):
+            # TODO: after each packet wait for the server's response before sending
+            # the next one, instead of with a fixed delay as we do now
+            await asyncio.sleep(0.05)
             print(f'sending image packet {index+1}/{len(printCommands)}')
-            self.send_packet(packet)
-            # print(f'({len(packet)}) {self.prettify_bytearray(packet[:40])}')
+            await self.send_packet(packet)
 
+async def main():
+    instax = InstaxBle()
+    # instax.enable_printing()  # uncomment this line to enable actual printing
+    await instax.connect()
+    await instax.send_led_pattern(LedPatterns.pulseGreen)
+    await instax.print_image('example.jpg')
+    await instax.disconnect()
 
 if __name__ == '__main__':
-    instax = InstaxBle()
-    # uncomment the next line to enable actual printing
-    # instax.enable_printing()
-    instax.connect()
-    if instax.isConnected:
-        instax.send_led_pattern([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
-        instax.print_image('example.jpg')
-    instax.disconnect()
+    asyncio.run(main())
