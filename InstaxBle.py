cb162cb185b84067c93a72d037276db14367a9d2
javl <mail@jaspervanloenen.com>
2023-03-25 17:04:59 +0100

    update main branch

--- InstaxBle.py
+++ InstaxBle.py
@@ -1,48 +1,122 @@
 #!/usr/bin/env python3
 
-from EventType import EventType
-from struct import pack
-import asyncio
-from bleak import BleakScanner
-import socket
+from math import ceil
+from struct import pack, unpack_from
+from time import sleep
+from Types import EventType
+import argparse
+import LedPatterns
+import simplepyble
+import sys
+
 
 class InstaxBle:
-    def __init__(self, deviceAddress=None, deviceName=None, printEnabled=False, printerName=None):
+    def __init__(self,
+                 device_address=None,
+                 print_enabled=False,
+                 dummy_printer=True,
+                 verbose=False,
+                 quiet=False):
         """
         Initialize the InstaxBle class.
+        deviceAddress: if specified, will only connect to a printer with this address.
         printEnabled: by default, actual printing is disabled to prevent misprints.
-        printerName: if specified, will only connect to a printer with this name.
         """
-        self.printEnabled = printEnabled
-        self.isConnected = False
-        self.device = None
-        self.deviceAddress = deviceAddress
-        self.deviceName = deviceName
-        self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self.printEnabled = print_enabled
+        self.peripheral = None
+        self.deviceAddress = device_address
+        self.dummyPrinter = dummy_printer
+        self.quiet = quiet
+        self.verbose = verbose if not self.quiet else False
+        self.serviceUUID = '70954782-2d83-473d-9e5f-81e1d02d5273'
+        self.writeCharUUID = '70954783-2d83-473d-9e5f-81e1d02d5273'
+        self.notifyCharUUID = '70954784-2d83-473d-9e5f-81e1d02d5273'
+        self.packetsForPrinting = []
+        self.waitingForResponse = False
+
+        adapters = simplepyble.Adapter.get_adapters()
+        if len(adapters) == 0:
+            if not self.quiet:
+                sys.exit("No bluetooth adapters found (are they enabled?)")
+            else:
+                sys.exit()
+        if len(adapters) > 1 and self.verbose:
+            print(f"Found multiple adapters: {', '.join([adapter.identifier() for adapter in adapters])}")
+            print(f"Using the first one: {adapters[0].identifier()}")
+        self.adapter = adapters[0]
+
+    def parse_response(self, packet):
+        """ Parse the response packet and print the result """
+        # todo: create parsers for the different types of responses
+        return
+
+    def notification_handler(self, packet):
+        """ Gets called whenever the printer replies and handles parsing the received data """
+        print('at handler')
+        if self.verbose:
+            print('Notification handler:')
+            print(f'\t{self.prettify_bytearray(packet[:40])}')
+        if not self.quiet:
+            if len(packet) < 8:
+                print(f"\tError: response packet size should be >= 8 (was {len(packet)})!")
+            elif not self.validate_checksum(packet):
+                print("\tResponse packet checksum was invalid!")
+
+        header, length, op1, op2 = unpack_from('<HHBB', packet)
+        # print('\theader: ', header, '\t', self.prettify_bytearray(packet[0:2]))
+        # print('\tlength: ', length, '\t', self.prettify_bytearray(packet[2:4]))
+        # print('\top1: ', op1, '\t\t', self.prettify_bytearray(packet[4:5]))
+        # print('\top2: ', op2, '\t\t', self.prettify_bytearray(packet[5:6]))
+
+        if self.verbose:
+            try:
+                event = EventType((op1, op2))
+            except Exception:
+                event = f"Unknown event: ({op1}, {op2})"
+            print('\tevent: ', event)
+
+        self.parse_response(packet)
+
+        print("packets to go: ", len(self.packetsForPrinting))
+        if len(self.packetsForPrinting) > 0:
+            packet = self.packetsForPrinting.pop(0)
+            self.send_packet(packet)
 
     def connect(self, timeout=0):
-        """ Connect to the printer. Quit trying after timeout seconds. """
-        self.isConnected = False
-        if self.deviceAddress is None:
-            self.device = asyncio.run(self.find_device(timeout=timeout))
-            if self.device:
-                print(f'found device {self.device.name}, connecting...')
-                self.deviceAddress = self.device.address
-            else:
-                print('no device found')
-                exit()
+        """ Connect to the printer. Stops trying after <timeout> seconds. """
+        if self.dummyPrinter:
+            return
 
-        try:
-            print(f'connecting to device {self.deviceAddress}')
-            self.sock.connect((self.deviceAddress, 6))
-            self.isConnected = True
-            print('connected')
-        except Exception as e:
-            print(f'connection failed: {e}')
+        self.peripheral = self.find_device(timeout=timeout)
+        if self.peripheral:
+            try:
+                if self.verbose:
+                    print(f"\n\nConnecting to: {self.peripheral.identifier()} [{self.peripheral.address()}]")
+                self.peripheral.connect()
+            except Exception as e:
+                if not self.quiet:
+                    print('error on connecting: ', e)
 
-    def close(self):
-        if self.isConnected:
-            self.sock.close()
+            if self.peripheral.is_connected():
+                if self.verbose:
+                    print(f"Connected (mtu: {self.peripheral.mtu()})")
+                    print('Attaching notification_handler')
+                try:
+                    self.peripheral.notify(self.serviceUUID, self.notifyCharUUID, self.notification_handler)
+                except Exception as e:
+                    if not self.quiet:
+                        print('error on attaching notification_handler: ', e)
+
+    def disconnect(self):
+        """ Disconnect from the printer (if connected) """
+        print('at disconnect')
+        if self.dummyPrinter:
+            return
+        if self.peripheral:
+            print('start disconnect')
+            if self.peripheral.is_connected():
+                self.peripheral.disconnect()
+            print('disconnect done')
 
     def enable_printing(self):
         """ Enable printing. """
@@ -52,40 +126,50 @@ class InstaxBle:
         """ Disable printing. """
         self.printEnabled = False
 
-    async def find_device(self, timeout=0, mode='ANDROID'):
+    def find_device(self, timeout=0):
         """" Scan for our device and return it when found """
-        print('Looking for instax printer...')
+        if self.verbose:
+            print('Looking for instax printer...')
         secondsTried = 0
         while True:
-            devices = await BleakScanner.discover(timeout=1)
-            for device in devices:
-                # if (self.printerName is None and dev.name.startswith('INSTAX-') and dev.name.endswith(mode)) or \
-                if (self.deviceName is None and device.name.startswith('INSTAX-') and device.name.endswith(f'({mode})')) or \
-                   device.name == self.deviceName or device.address == self.deviceAddress:
-                    return device
+            self.adapter.scan_for(2000)
+            peripherals = self.adapter.scan_get_results()
+            for peripheral in peripherals:
+                foundName = peripheral.identifier()
+                foundAddress = peripheral.address()
+                if self.verbose:
+                    print(f"Found: {foundName} [{foundAddress}]")
+                if (self.deviceAddress is None and foundName.startswith('INSTAX-')) or \
+                   foundAddress == self.deviceAddress:
+                    if foundAddress.startswith('FA:AB:BC'):  # start of IOS endpooint
+                        # to convert to ANDROID endpoint, replace 'FA:AB:BC' to '88:B4:36')
+                        if peripheral.is_connectable():
+                            return peripheral
+                        elif not self.quiet:
+                            print(f"Printer at {foundAddress} is not connectable")
             secondsTried += 1
             if timeout != 0 and secondsTried >= timeout:
                 return None
 
     def create_color_payload(self, colorArray, speed, repeat, when):
         """
-        Create a payload for a color pattern.
-        colorArray: array of RGB values to use in animation, e.g. [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
-        speed: time per frame/color. Higher is slower animation
-        repeat: 0 = don't repeat (so play once), 1-254 = times to repeat, 255 = repeat forever
-        when: 0 = normal, 1 = on print, 2 = on print completion, 3 = pattern switch
+        Create a payload for a color pattern. See send_led_pattern for details.
         """
-
         payload = pack('BBBB', when, len(colorArray), speed, repeat)
         for color in colorArray:
             payload += pack('BBB', color[0], color[1], color[2])
         return payload
 
     def send_led_pattern(self, pattern, speed=5, repeat=255, when=0):
-        """ Send a LED pattern to the Instax printer. """
+        """ Send a LED pattern to the Instax printer.
+            colorArray: array of BGR(!) values to use in animation, e.g. [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
+            speed: time per frame/color: higher is slower animation
+            repeat: 0 = don't repeat (so play once), 1-254 = times to repeat, 255 = repeat forever
+            when: 0 = normal, 1 = on print, 2 = on print completion, 3 = pattern switch """
         payload = self.create_color_payload(pattern, speed, repeat, when)
         packet = self.create_packet(EventType.LED_PATTERN_SETTINGS, payload)
-        return self.send_packet(packet)
+        print("size: ", len(packet))
+        self.send_packet(packet)
 
     def prettify_bytearray(self, value):
         """ Helper funtion to convert a bytearray to a string of hex values. """
@@ -100,7 +184,7 @@ class InstaxBle:
         if isinstance(eventType, EventType):  # allows passing in an event or a value directly
             eventType = eventType.value
 
-        header = b'\x41\x62'  # Ab from client to printer, Ba from printer to client
+        header = b'\x41\x62'  # 'Ab' means from client to printer, responses from printer start with 'aB'
         opCode = bytes([eventType[0], eventType[1]])
         packetSize = pack('>H', 7 + len(payload))
         packet = header + packetSize + opCode + payload
@@ -111,38 +195,36 @@ class InstaxBle:
         """ Validate the checksum of a packet. """
         return (sum(packet) & 255) == 255
 
-    # async def send_color(self):
-    #     """ send a color pattern """
-    #     im = Image.open('gradient.jpg')
-    #     px = im.load()
-    #     size = im.size if im._getexif().get(274, 0) < 5 else im.size[::-1]
-    #     colorArray = []
-    #     for x in range(0, size[0], 20):
-    #         color = im.getpixel((x, 0))
-    #         colorArray.append([color[0], color[1], color[2]])
-
-    #     payload = self.create_color_payload(colorArray, 10, 255)
-    #     packet = self.create_packet(EventType.LED_PATTERN_SETTINGS, payload)
-
-    #     sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    #     sock.connect((self.device.address, 6))
-    #     sock.send(packet)
-    #     resp = sock.recv(8)
-    #     sock.close()
-    #     return resp
-
     def send_packet(self, packet):
-        """ Send a packet to the printer. """
-        # print("sending: ", self.prettify_bytearray(packet))
-        if self.isConnected:
-            self.sock.send(packet)
-            # print('sent')
-            response = self.sock.recv(64)
-            # self.sock.close()
-            return response
+        """ Send a packet to the printer """
+        print("go send")
+        if not self.dummyPrinter and not self.quiet:
+            if not self.peripheral:
+                print("no peripheral to send packet to")
+            elif not self.peripheral.is_connected():
+                print("peripheral not connected")
 
-    # TODO: printer doesn't seem to respond to this
-    # def shut_down(self):
+        header, length, op1, op2 = unpack_from('<HHBB', packet)
+        try:
+            event = EventType((op1, op2))
+        except Exception:
+            event = 'Unknown event'
+
+        if self.verbose:
+            print('sending eventtype: ', event)
+
+        smallPacketSize = 182
+        numberOfParts = ceil(len(packet) / smallPacketSize)
+        print("number of packets to send: ", numberOfParts)
+        for subPartIndex in range(numberOfParts):
+            print((subPartIndex + 1), '/', numberOfParts)
+            subPacket = packet[subPartIndex * smallPacketSize:subPartIndex * smallPacketSize + smallPacketSize]
+
+            if not self.dummyPrinter:
+                self.peripheral.write_command(self.serviceUUID, self.writeCharUUID, subPacket)
+
+    # TODO: printer doesn't seem to respond to this?
+    # async def shut_down(self):
     #     """ Shut down the printer. """
     #     packet = self.create_packet(EventType.SHUT_DOWN)
     #     return self.send_packet(packet)
@@ -156,50 +238,116 @@ class InstaxBle:
                 imgdata = bytearray(image.read())
             return imgdata
         except Exception as e:
-            print('Error loading image: ', e)
+            if not self.quiet:
+                print('Error loading image: ', e)
 
     def print_image(self, imgSrc):
-        """ print an image. Either pass a path to an image or a bytearray"""
+        """
+        print an image. Either pass a path to an image (as a string) or pass
+        the bytearray to print directly
+        """
+        imgData = imgSrc
         if isinstance(imgSrc, str):  # if it's a path, load the image contents
             imgData = self.image_to_bytes(imgSrc)
-        else:
-            imgData = imgSrc
 
-        printCommands = [
+        self.packetsForPrinting = [
             self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_START, b'\x02\x00\x00\x00\x00\x00' + pack('>H', len(imgData)))
         ]
 
-        # divide image data up into chunks of 900 bytes and pad the last chunk with zeroes if needed
-        imgDataChunks = [imgData[i:i + 900] for i in range(0, len(imgData), 900)]
-        if len(imgDataChunks[-1]) < 900:
-            imgDataChunks[-1] = imgDataChunks[-1] + bytes(900 - len(imgDataChunks[-1]))
+        # divide image data up into chunks of <chunkSize> bytes and pad the last chunk with zeroes if needed
+        # chunkSize = 900
+        chunkSize = 900
+        imgDataChunks = [imgData[i:i + chunkSize] for i in range(0, len(imgData), chunkSize)]
+        if len(imgDataChunks[-1]) < chunkSize:
+            imgDataChunks[-1] = imgDataChunks[-1] + bytes(chunkSize - len(imgDataChunks[-1]))
 
+        # create a packet from each of our chunks, this includes adding the chunk number
         for index, chunk in enumerate(imgDataChunks):
             imgDataChunks[index] = pack('>I', index) + chunk  # add chunk number as int (4 bytes)
-            printCommands.append(self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_DATA, pack('>I', index) + chunk))
+            self.packetsForPrinting.append(self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_DATA, imgDataChunks[index]))
+
+        self.packetsForPrinting.append(self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_END))
 
         if self.printEnabled:
-            printCommands.extend([
-                self.create_packet(EventType.PRINT_IMAGE_DOWNLOAD_END),
-                self.create_packet(EventType.PRINT_IMAGE),
-                self.create_packet((0, 2), b'\x02'),
-            ])
-        else:
-            print("Printing is disabled. Sending all packets except for PRINT_IMAGE command")
+            self.packetsForPrinting.append(self.create_packet(EventType.PRINT_IMAGE))
+            self.packetsForPrinting.append(self.create_packet((0, 2), b'\x02'))
+        elif not self.quiet:
+            print("Printing is disabled, sending all packets except the actual print command")
 
-        for index, packet in enumerate(printCommands):
-            print(f'sending image packet {index+1}/{len(printCommands)}')
+        # for packet in self.packetsForPrinting:
+        #     print(self.prettify_bytearray(packet))
+        # exit()
+        # send the first packet from our list, the packet handler will take care of the rest
+
+        if not self.dummyPrinter:
+            packet = self.packetsForPrinting.pop(0)
             self.send_packet(packet)
+            print("entering wait loop")
+            try:
+                while len(self.packetsForPrinting) > 0:
+                    sleep(0.1)
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+
+    def print_services(self):
+        """ Get and display and overview of the printer's services and characteristics """
+        if self.verbose:
+            print("Successfully connected, listing services...")
+        services = self.peripheral.services()
+        service_characteristic_pair = []
+        for service in services:
+            for characteristic in service.characteristics():
+                service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
+
+        for i, (service_uuid, characteristic) in enumerate(service_characteristic_pair):
+            print(f"{i}: {service_uuid} {characteristic}")
+
+    def get_function_info(self):
+        """ Get and display the printer's function info """
+        if self.verbose:
+            print("Getting function info...")
+        packet = self.create_packet(EventType.SUPPORT_FUNCTION_INFO, b'0x02')
+        self.send_packet(packet)
+
+
+def main(args={}):
+    """ Main script used as an example of how to control the printer """
+    instax = InstaxBle(**args)
+    try:
+        # By default the final print command does not get send to the printer
+        # Uncomment the next line, or pass --print-enabled when calling the script
+        # to enable printing
+        # instax.enable_printing()
+
+        instax.connect()
+        instax.get_function_info()
+        # print("Successfully connected, listing services...")
+        # instax.print_services()
+
+        # Set a rainbox effect to be shown while printing and a pulsatating
+        # green effect when printing is done
+        instax.send_led_pattern(LedPatterns.rainbow, when=1)
+        instax.send_led_pattern(LedPatterns.pulseGreen, when=2)
+
+        # send the image (.jpg) to the printer
+        instax.print_image('example.jpg')
+
+        # all done, disconnect
+        instax.disconnect()
+
+    except Exception:
+        # when something goes wrong try to disconnect before exiting, otherwise
+        # you might have to restart the printer before you can connect again
+        instax.disconnect()
 
 
 if __name__ == '__main__':
-    instax = InstaxBle()
-    # or specify your device address to skip searching
-    # instax = InstaxBle(deviceAddress='88:B4:36:xx:xx:xx')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--device-address')
+    parser.add_argument('-p', '--print-enabled', action='store_true')
+    parser.add_argument('-d', '--dummy-printer', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-q', '--quiet', action='store_true')
+    args = parser.parse_args()
 
-    # uncomment the next line to enable actual printing
-    # instax.enable_printing()
-    instax.connect()
-    if instax.isConnected:
-        instax.send_led_pattern([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
-        instax.print_image('example.jpg')
+    main(vars(args))
